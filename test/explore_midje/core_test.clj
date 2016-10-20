f65a7aa46d4072aea4d005286b34659ca8e1980d
Giles Bowkett <gilesb@gmail.com>
2016-10-20 11:19:34 -0600

    utter 'hi world' of midje-ing

--- test/explore_midje/core_test.clj
+++ test/explore_midje/core_test.clj
@@ -1,7 +1,11 @@
 (ns explore-midje.core-test
+  (:use midje.sweet)
   (:require [clojure.test :refer :all]
+            [clojure.string :as str]
             [explore-midje.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(fact "`split` splits strings on regular expressions and returns a vector"
+  (str/split "a/b/c" #"/") => ["a" "b" "c"]
+  (str/split "" #"irrelevant") => [""]
+  (str/split "no regexp matches" #"a+\s+[ab]") => ["no regexp matches"])
+
