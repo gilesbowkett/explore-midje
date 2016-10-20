fb6e8f7c7f2813d101a7144281bb901bfb1567ae
Giles Bowkett <gilesb@gmail.com>
2016-10-20 12:18:24 -0600

    more accurate var name

--- src/explore_midje/core.clj
+++ src/explore_midje/core.clj
@@ -1,9 +1,9 @@
 (ns explore-midje.core)
 
-(defn manual-flatten [array]
-  (let [x  (first array)
-        xs (rest array)]
-    (cond (empty? array) nil
+(defn manual-flatten [coll]
+  (let [x  (first coll)
+        xs (rest coll)]
+    (cond (empty? coll) nil
           (sequential? x)
             (concat (manual-flatten x) (manual-flatten xs))
           (number? x)
