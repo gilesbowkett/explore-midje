4ee7577a8006c08e115b0f9fe760d741f1aab326
Giles Bowkett <gilesb@gmail.com>
2016-10-20 12:28:39 -0600

    restore :else because of its universality

--- src/explore_midje/core.clj
+++ src/explore_midje/core.clj
@@ -6,6 +6,6 @@
     (cond (empty? coll) nil
           (sequential? x)
             (concat (manual-flatten x) (manual-flatten xs))
-          (number? x)
+          :else
             (cons x (manual-flatten xs)))))
 
--- test/explore_midje/core_test.clj
+++ test/explore_midje/core_test.clj
@@ -1,7 +1,6 @@
 (ns explore-midje.core-test
   (:use midje.sweet)
   (:require [clojure.test :refer :all]
-            [clojure.string :as str]
             [explore-midje.core :refer :all]))
 
 (fact "`manual-flatten` manually flattens arrays"
@@ -11,4 +10,7 @@
 
 (fact "`manual-flatten` also works on maps"
   (manual-flatten {1 2 3 4}) => [1 2 3 4])
+
+(fact "and arrays of strings"
+  (manual-flatten ["a" ["b" ["c" ["d"]]]]) => ["a" "b" "c" "d"])
 
