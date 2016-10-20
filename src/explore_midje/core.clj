ae11ba78ddd86ef7b08a159eb8263e7a1013a988
Giles Bowkett <gilesb@gmail.com>
2016-10-20 11:49:28 -0600

    more idiomatic var name

--- src/explore_midje/core.clj
+++ src/explore_midje/core.clj
@@ -1,11 +1,11 @@
 (ns explore-midje.core)
 
 (defn manual-flatten [something]
-  (let [first-thing (first something)
-        xs          (rest something)]
+  (let [x  (first something)
+        xs (rest something)]
     (cond (empty? something) nil
-          (sequential? first-thing)
-            (concat (manual-flatten first-thing) (manual-flatten xs))
+          (sequential? x)
+            (concat (manual-flatten x) (manual-flatten xs))
           :else
-            (cons first-thing (manual-flatten xs)))))
+            (cons x (manual-flatten xs)))))
 
